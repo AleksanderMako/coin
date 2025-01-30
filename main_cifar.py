94bb71cc01b8f586692aee59473c6b4e5e7bc4f2
AleksanderMako <amako9281@gmail.com>
2025-01-30 21:08:18 +0000

    add git ignore

--- main_cifar.py
+++ main_cifar.py
@@ -12,7 +12,7 @@ from torchvision.utils import save_image
 from training import Trainer
 import cifar10loader 
 
-
+# python main_cifar.py -ld cifar_10_full_dataset -fd -nl 5 -lss 20 -ni 250
 parser = argparse.ArgumentParser()
 parser.add_argument("-ld", "--logdir", help="Path to save logs", default=f"/tmp/{getpass.getuser()}")
 parser.add_argument("-ni", "--num_iters", help="Number of iterations to train for", type=int, default=1000)
